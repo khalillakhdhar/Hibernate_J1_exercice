87b001cb9e54f20fdb66146ff19d3e746fb8dffd
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-08 15:44:23 +0100

    Update User.java

--- src/main/java/com/hibernate/model/User.java
+++ src/main/java/com/hibernate/model/User.java
@@ -1,5 +1,123 @@
 package com.hibernate.model;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author TPC
+ */
+@Entity
+@Table(name = "user")
+
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    private Integer id;
+    @Basic(optional = false)
+    private String email;
+
+    private String mdp;
+    @Basic(optional = false)
+    private String nom;
+    @Basic(optional = false)
+    private String telephone;
+    @Basic(optional = false)
+    private String adresse;
+    @Basic(optional = false)
+    @Column(name = "grade")
+    private String grade;
+
+    public User() {
+    }
+
+    public User(Integer id) {
+        this.id = id;
+    }
+
+    public User(Integer id, String email, String mdp, String nom, String telephone, String adresse, String grade) {
+        this.id = id;
+        this.email = email;
+        this.mdp = mdp;
+        this.nom = nom;
+        this.telephone = telephone;
+        this.adresse = adresse;
+        this.grade = grade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+ 
+    
 }
